80ea540113fba49c647ec852980f5ba723da40f7
reason <reason@mofunsky.com>
2015-11-17 17:00:16 +0800

    update data path

--- src/main/scala/inputdata/MovieLensDataHolder.scala
+++ src/main/scala/inputdata/MovieLensDataHolder.scala
@@ -4,43 +4,42 @@ import org.apache.spark.mllib.recommendation.Rating
 import org.apache.spark.rdd.RDD
 
 /**
- * Created by Ondra Fiedler on 27.8.14.
- */
+  * Created by Ondra Fiedler on 27.8.14.
+  */
 
 /**
- * Loads and holds the MovieLens data (http://grouplens.org/datasets/movielens/)
- * @param dataDirectoryPath Path to the directory with MovieLens data. This directory contain file "ratings.dat" with ratings and file "movies.dat" with mapping between movie IDs and names
- */
+  * Loads and holds the MovieLens data (http://grouplens.org/datasets/movielens/)
+  * @param dataDirectoryPath Path to the directory with MovieLens data. This directory contain file "ratings.dat" with ratings and file "movies.dat" with mapping between movie IDs and names
+  */
 class MovieLensDataHolder(dataDirectoryPath: String) extends DataHolder with Serializable {
+
 
   protected val ratings = loadRatingsFromADirectory()
   protected val productsIDsToNameMap = loadIDsToProductnameMapFromADirectory(dataDirectoryPath)
 
+  check()
+
   protected def loadRatingsFromADirectory(): RDD[Rating] = {
 
-    val ratings = spark.sparkEnvironment.sc.textFile(dataDirectoryPath + "/base_data/*/*").map { line =>
+    val ratings = spark.sparkEnvironment.sc.textFile(dataDirectoryPath + "/ratings.dat").map { line =>
       val fields = line.split("::")
       // format: Rating(userID, movieID, rating)
-      try {
-        Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble)
-      } catch {
-        case e: Exception => Rating(0, 0, 0.0)
-      }
+      (Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble))
     }
-
-    ratings.filter(r => productsIDsToNameMap.contains(r.product))
+    ratings
   }
 
   protected def loadIDsToProductnameMapFromADirectory(dataDirectoryPath: String): Map[Int, String] = {
-    val movies = spark.sparkEnvironment.sc.textFile(dataDirectoryPath + "/mysql_data/user/*/*").map { line =>
-      val fields = line.split(":")
+    val movies = spark.sparkEnvironment.sc.textFile(dataDirectoryPath + "/movies.dat").map { line =>
+      val fields = line.split("::")
       // format: (movieID, movieName)
-      try {
-        (fields(0).toInt, fields(1))
-      } catch {
-        case e: Exception => (0, " ")
-      }
+      (fields(0).toInt, fields(1))
     }.collect.toMap
     movies
+  }
+
+  protected def check() = {
+    val wrong = ratings.filter(r => (r.rating < 0 || (r.rating > 5) || (!productsIDsToNameMap.contains(r.product))))
+    if (wrong.count() != 0) throw new WrongInputDataException
   }
 }
